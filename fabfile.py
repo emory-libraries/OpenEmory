fdce9dca530ecfb78e9885636260b7af9221a706
Ben Ranker <branker@emory.edu>
2011-08-11 17:25:46 +0000

    add fabric with targets for common dev/test tasks

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from fabric.api import env, lcd, local, prefix
+
+# omit these from the test coverage report
+env.omit_coverage = ','.join([
+    os.environ['VIRTUAL_ENV'],
+    'openemory/manage.py',
+    'openemory/settings.py',
+    'openemory/localsettings.py',
+    ])
+
+def all_deps():
+    '''Locally install all dependencies.'''
+    local('pip install -r pip-install-req.txt -r pip-dev-req.txt')
+
+def test():
+    '''Locally run all tests.'''
+    if os.path.exists('test-results'):
+        shutil.rmtree('test-results')
+
+    local('coverage run --branch openemory/manage.py test --noinput')
+    local('coverage xml --omit=%(omit_coverage)s' % env)
+
+def doc():
+    '''Locally build documentation.'''
+    with lcd('doc'):
+        local('make clean html')
