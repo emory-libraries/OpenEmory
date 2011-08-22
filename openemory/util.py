68b4514c86cadb46b2dc8323fa09beeed61a4b35
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-08-22 22:53:51 +0000

    add util with md5sum method

---
+++ openemory/util.py
@@ -0,0 +1,21 @@
+'''
+Common utility methods used elsewhere in the site.
+
+'''
+
+import hashlib
+
+def md5sum(filename):
+    '''Calculate and returns an MD5 checksum for the specified file.  Any file
+    errors (non-existent file, read error, etc.) are not handled here but should
+    be caught where this method is called.
+
+    :param filename: full path to the file for which a checksum should be calculated
+    :returns: hex-digest formatted MD5 checksum as a string
+    '''
+    # copied from keep.common.utils
+    md5 = hashlib.md5()
+    with open(filename,'rb') as f:
+        for chunk in iter(lambda: f.read(128*md5.block_size), ''):
+             md5.update(chunk)
+    return md5.hexdigest()
