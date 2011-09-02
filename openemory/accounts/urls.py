ae444b08429cbb99ede3b54c72a11b77ef1b175c
Ben Ranker <branker@emory.edu>
2011-09-02 14:13:56 +0000

    local usernames can have capitals, too

--- openemory/accounts/urls.py
+++ openemory/accounts/urls.py
@@ -4,5 +4,5 @@ from openemory.accounts import views
 urlpatterns = patterns('',
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
-    url(r'^(?P<username>[a-z0-9]+)/$', views.profile, name='profile'),
+    url(r'^(?P<username>[a-zA-Z0-9]+)/$', views.profile, name='profile'),
 )
