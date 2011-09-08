bee3570d74ce0b054d3bafecdcfd5948a18f7380
Ben Ranker <branker@emory.edu>
2011-09-08 16:52:35 +0000

    process article chunks until at least one harvest is found. #40

--- openemory/harvest/management/commands/fetch_pmc_metadata.py
+++ openemory/harvest/management/commands/fetch_pmc_metadata.py
@@ -1,11 +1,14 @@
-import logging
 from collections import defaultdict
+import logging
+from optparse import make_option
+import os
+
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 from eulxml import xmlmap
+
 from openemory.harvest.entrez import EFetchResponse
 from openemory.harvest.models import OpenEmoryEntrezClient, HarvestRecord
-from optparse import make_option
-from os import path
 
 logger = logging.getLogger(__name__)
 
@@ -30,47 +33,53 @@ class Command(BaseCommand):
                     help='Number of articles to fetch at a time.'),
         )
     
-    def handle(self, simulate, count, *args, **options):
+    def handle(self, *args, **options):
         self.verbosity = int(options['verbosity'])    # 1 = normal, 0 = minimal, 2 = all
         self.v_normal = 1
 
-        if simulate:
-            # simulation mode requested; load fixture response
-            articles = self.simulated_response()
-            if self.verbosity >= self.v_normal:
-                self.stdout.write('Simulation mode requested; using static fixture content\n')
-        else:
-            self.entrez = OpenEmoryEntrezClient()
-            article_qs = self.entrez.get_emory_articles() 
-            articles = article_qs[:count]
-
-        stats = defaultdict(int)            
-        for article in articles:
-            stats['articles'] += 1
-
+        stats = defaultdict(int)
+        for article_chunk in self.article_chunks(**options):
             if self.verbosity > self.v_normal:
-                self.stdout.write('Processing [%s] "%s"\n' % \
-                                  (article.docid, article.article_title))
-            
-            if HarvestRecord.objects.filter(pmcid=article.docid).exists():
-                if self.verbosity >= self.v_normal:
-                    self.stdout.write('[%s] has already been harvested; skipping\n' \
-                                      % (article.docid,))
-                continue
+                self.stdout.write('Starting article chunk.\n')
+
+            for article in article_chunk:
+                stats['articles'] += 1
+
+                if self.verbosity > self.v_normal:
+                    self.stdout.write('Processing [%s] "%s"\n' % \
+                                      (article.docid, article.article_title))
                 
-            if article.identifiable_authors():
-                try:
-                    HarvestRecord.init_from_fetched_article(article)
-                    stats['harvested'] += 1
-                except Exception as err:
-                    self.stdout.write('Error creating record from article: %s\n' % err)
-                    stats['errors'] += 1
+                if HarvestRecord.objects.filter(pmcid=article.docid).exists():
+                    if self.verbosity >= self.v_normal:
+                        self.stdout.write('[%s] has already been harvested; skipping\n' \
+                                          % (article.docid,))
+                    continue
                     
-            else:
-                if self.verbosity >= self.v_normal:
-                    self.stdout.write('[%s] has no identifiable authors; skipping\n' \
-                                      % (article.docid,))
-                stats['noauthor'] += 1
+                if article.identifiable_authors():
+                    try:
+                        HarvestRecord.init_from_fetched_article(article)
+                        stats['harvested'] += 1
+                    except Exception as err:
+                        self.stdout.write('Error creating record from article: %s\n' % err)
+                        stats['errors'] += 1
+                        
+                else:
+                    if self.verbosity >= self.v_normal:
+                        self.stdout.write('[%s] has no identifiable authors; skipping\n' \
+                                          % (article.docid,))
+                    stats['noauthor'] += 1
+
+            # if we haven't gotten any articles yet, keep going. if we have,
+            # then we're done.
+            if stats['harvested']:
+                if self.verbosity > self.v_normal:
+                    self.stdout.write('Harvested articles; stopping.')
+                break
+
+        else:
+            if self.verbosity > self.v_normal:
+                self.stdout.write('Processed all chunks.')
+            
 
         # summarize what was done
         if self.verbosity >= self.v_normal:
@@ -83,10 +92,23 @@ class Command(BaseCommand):
                 self.stdout.write('Articles skipped (no identifiable authors): %(noauthor)d\n' \
                                   % stats)
 
-
+    def article_chunks(self, simulate, count, **kwargs):
+        if simulate:
+            # simulation mode requested; load fixture response
+            if self.verbosity >= self.v_normal:
+                self.stdout.write('Simulation mode requested; using static fixture content\n')
+            yield self.simulated_response()
+        else:
+            entrez = OpenEmoryEntrezClient()
+            qs = entrez.get_emory_articles() 
+            paginator = Paginator(qs, count)
+            for i in paginator.page_range:
+                page = paginator.page(i)
+                yield page.object_list
+        
 
     def simulated_response(self):
-        article_path = path.join(path.dirname(__file__), '..',
+        article_path = os.path.join(os.path.dirname(__file__), '..',
             '..', 'fixtures', 'efetch-retrieval-from-hist.xml')
         fetch_response = xmlmap.load_xmlobject_from_file(article_path,
                                                          xmlclass=EFetchResponse)
