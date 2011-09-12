1b9844244001dd447d58f7e6e3b0e721f6e082a7
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-09-12 17:12:04 +0000

    add javascript/html/css to harvest queue page for a working ignore button #34

--- openemory/harvest/static/js/queue.js
+++ openemory/harvest/static/js/queue.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     // pick up the csrf token for use in all ajax requests
     var csrftoken = $("#csrftoken input").attr("value");
 
+    // NOTE: There is some overlap in ingest/ignore functionality
+    // below (particularly success/error handling and message
+    // display), but it is not obvious how to combine them.
+
     // configure ingest buttons to POST pmcid when clicked
     $(".ingest").click(function() {
         var data = { pmcid: $(this).find(".pmcid").html()};
@@ -16,19 +20,45 @@ $(document).ready(function () {
             headers: {'X-CSRFTOKEN': csrftoken},
             success: function(data, status, xhr) {
                 msg.html('Success');
-                msg.removeClass(errclass).addClass(success_class).show();
-                // hide the whole queue item on success
-                msg.parents('.queue-item').delay(1500).fadeOut();
+                msg.removeClass(errclass).addClass(success_class).show().delay(1500).fadeOut();
+                // change queue item class on success so display can be updated
+                msg.parents('.queue-item').addClass('ingested');
             },
             error: function(data, status, xhr) {
-
                 msg.html('Error: ' + data.responseText);
                 msg.removeClass(success_class).addClass(errclass).show();
-                // should error message disappear ? 
-                //msg.delay(1500).fadeOut();
+                // NOTE: not fading error message out, since it may
+                // need to be reported
             }
 
         });
         return false;
     });
+
+    // configure ignore buttons to DELETE specified url when clicked
+    $(".ignore").click(function() {
+        var msg = $(this).parent().find('.message');
+        var errclass = 'error-msg';
+        var success_class = 'success-msg';
+        $.ajax({
+            type: 'DELETE',
+            url: $(this).attr('href'),
+            headers: {'X-CSRFTOKEN': csrftoken},
+            success: function(data, status, xhr) {
+                msg.html('Success');
+                msg.removeClass(errclass).addClass(success_class).show().delay(1500).fadeOut();
+                // change queue item on success so display can be updated
+                msg.parents('.queue-item').addClass('ignored');
+            },
+            error: function(data, status, xhr) {
+                msg.html('Error: ' + data.responseText);
+                msg.removeClass(success_class).addClass(errclass).show();
+            }
+
+        });
+        return false;
+    });
+
 });
+
+
---
+++ sitemedia/js/local.js
@@ -0,0 +1,6 @@
+/* make console.log not an error even if console is not available */
+if (!window.console) console = {};
+console.log = console.log || function(){};
+console.warn = console.warn || function(){};
+console.error = console.error || function(){};
+console.info = console.info || function(){};
