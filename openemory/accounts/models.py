ee38073a1c23d91fe2c706159c7942ca9dd19ada
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2011-09-21 17:29:44 +0000

    remove user profile post-save signal handler (now handled by eullocal)

--- openemory/accounts/models.py
+++ openemory/accounts/models.py
@@ -7,12 +7,3 @@ from taggit.managers import TaggableManager
 class UserProfile(AbstractEmoryLDAPUserProfile):
     user = models.OneToOneField(User)
     research_interests = TaggableManager()
-
-def _create_profile(sender, instance, created, **kwargs):
-    # create profile if this is a new User being created OR
-    # if profile does not yet exist
-    if created or not UserProfile.objects.filter(user=instance).exists():
-        profile = UserProfile(user=instance)
-        profile.save()
-models.signals.post_save.connect(_create_profile, sender=User)
-
