f4dcbbae8dd665312c2925009edcd145863c0d19
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-01-18 22:54:43 +0000

    make degrees available in django db-admin, for developer convenience

--- openemory/accounts/admin.py
+++ openemory/accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from openemory.accounts.models import Bookmark
+from openemory.accounts.models import Bookmark,  Degree
 
 
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('user', 'pid', 'display_tags')
 
 admin.site.register(Bookmark, BookmarkAdmin)
+
+class DegreeAdmin(admin.ModelAdmin):
+    list_display = ('holder', 'name', 'institution', 'year')
+    
+admin.site.register(Degree, DegreeAdmin)
