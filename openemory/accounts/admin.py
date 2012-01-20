6f2f2f49a07f4ed15ac56aeebaa99328b98e935f
Ben Ranker <branker@emory.edu>
2012-01-20 22:01:21 +0000

    add PositionAdmin

--- openemory/accounts/admin.py
+++ openemory/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from openemory.accounts.models import Bookmark,  Degree
+from openemory.accounts.models import Bookmark, Degree, Position, \
+        UserProfile
 
 
 class BookmarkAdmin(admin.ModelAdmin):
@@ -8,6 +9,13 @@ class BookmarkAdmin(admin.ModelAdmin):
 admin.site.register(Bookmark, BookmarkAdmin)
 
 class DegreeAdmin(admin.ModelAdmin):
+    # TODO: inline this in UserProfile admin
     list_display = ('holder', 'name', 'institution', 'year')
     
 admin.site.register(Degree, DegreeAdmin)
+
+class PositionAdmin(admin.ModelAdmin):
+    # TODO: inline this in UserProfile admin
+    list_display = ('holder', 'name')
+
+admin.site.register(Position, PositionAdmin)
