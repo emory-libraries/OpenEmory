ea96472956da05c08e9d7241a71ab4283ef50a31
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-01-24 15:16:34 +0000

    revise accounts urls to put departments at top-level, profile pages under /profiles/ #206

--- openemory/accounts/urls.py
+++ openemory/accounts/urls.py
@@ -4,20 +4,27 @@ from openemory.accounts import views
 urlpatterns = patterns('openemory.accounts.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^logout/$', 'logout', name='logout'),
-    # TODO: should be top-level /departments/, not /accounts/departments/
+    # department browse
     url(r'^departments/$', views.departments, name='list-departments'),
     url(r'^departments/(?P<id>[A-Z0-9]+)/$', views.view_department,
         name='department'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)/$', 'profile', name='profile'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)/edit/$', 'edit_profile', name='edit-profile'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)/name/$', 'user_name', name='user-name'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)/data/$', 'rdf_profile', name='profile-data'),
-    url(r'^(?P<username>[a-zA-Z0-9]+)/tags/$', views.profile_tags, name='profile-tags'),
-    url(r'^degree/(?P<mode>(institution|name))/autocomplete/$', views.degree_autocomplete,
-        name='degree-autocomplete'),                       
-    url(r'^grant/autocomplete/$', views.grant_autocomplete, name='grant-autocomplete'),
-    url(r'^interests/autocomplete/$', views.interests_autocomplete, name='interests-autocomplete'),
-    url(r'^interests/(?P<tag>[a-zA-Z0-9-_]+)/$', views.researchers_by_interest, name='by-interest'),
+    # profile pages
+    url(r'^profiles/(?P<username>[a-zA-Z0-9]+)/$', 'profile', name='profile'),
+    url(r'^profiles/(?P<username>[a-zA-Z0-9]+)/edit/$', 'edit_profile', name='edit-profile'),
+    url(r'^profiles/(?P<username>[a-zA-Z0-9]+)/name/$', 'user_name', name='user-name'),
+    url(r'^profiles/(?P<username>[a-zA-Z0-9]+)/data/$', 'rdf_profile', name='profile-data'),
+    url(r'^profiles/(?P<username>[a-zA-Z0-9]+)/tags/$', views.profile_tags, name='profile-tags'),
+    # profile-specific auto-complete views
+    url(r'^profiles/degrees/(?P<mode>(institution|name))/autocomplete/$',
+        views.degree_autocomplete, name='degree-autocomplete'),                       
+    url(r'^profiles/grants/autocomplete/$', views.grant_autocomplete,
+        name='grant-autocomplete'),
+    url(r'^profiles/interests/autocomplete/$', views.interests_autocomplete,
+        name='interests-autocomplete'),
+    url(r'^profiles/interests/(?P<tag>[a-zA-Z0-9-_]+)/$', views.researchers_by_interest,
+        name='by-interest'),
+
+    # article tagging views
     url(r'^tags/autocomplete/$', views.tags_autocomplete, name='tags-autocomplete'),
     url(r'^tags/(?P<pid>[^/]+)/$', views.object_tags, name='tags'),
     url(r'^tag/(?P<tag>[a-zA-z0-9-_]+)/$', views.tagged_items, name='tag'),
--- openemory/urls.py
+++ openemory/urls.py
@@ -8,13 +8,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'openemory.publication.views.recent_uploads',  name='site-index'),
     url(r'^publications/', include('openemory.publication.urls', namespace='publication')),
-    url(r'^accounts/', include('openemory.accounts.urls', namespace='accounts')),
     url(r'^harvest/', include('openemory.harvest.urls', namespace='harvest')),
     # django db admin
     url(r'^db-admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^db-admin/', include(admin.site.urls)),
     # indexdata
     url(r'^indexdata/', include('eulfedora.indexdata.urls', namespace='indexdata')),
+    # accounts app includes several top-level urls
+    url(r'^', include('openemory.accounts.urls', namespace='accounts')),
 )
 
 if settings.DEBUG:
