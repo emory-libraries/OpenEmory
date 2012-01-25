5ad89d59ef0f0e7ca16a893d316e303564bdfd7f
Alex Thomas <athom09@emory.edu>
2012-01-25 17:00:14 +0000

    #172  made query a little simpler and updated comments and doc string

--- openemory/accounts/backends.py
+++ openemory/accounts/backends.py
@@ -7,19 +7,21 @@ from openemory.accounts.models import EsdPerson, UserProfile
 class FacultyOrLocalAdminBackend(EmoryLDAPBackend):
     '''Customized authentication backend based on
     :class:`~eullocal.django.emory_ldap.backends.EmoryLDAPBackend`.
-    Only users who are designated as Faculty in ESD or local users who
-    are designated as superusers or Site Admins are allowed to log in.
+    Only users who are designated as Faculty, in ESD or local users who
+    are designated as superusers, Site Admins, or non-faculty with the
+    nonfaculty_profile flag set are allowed to log in.
     '''    
 
     def authenticate(self, username=None, password=None):
         # Only authenticate users who are flagged as faculty in ESD
-        # or local accounts with superuser permission
+        # or local accounts with superuser permission, 'Site Admin' role
+        # or nonfaculty_flag set
         if self.USER_MODEL.objects.filter(username=username)\
                .filter(Q(is_superuser=True) | Q(groups__name='Site Admin'))\
                .exists() or \
                EsdPerson.objects.filter(netid=username.upper(),
                                     person_type='F').exists() or \
-               UserProfile.objects.filter(user=User.objects.filter(username=username)).\
+               UserProfile.objects.filter(user__username=username).\
                filter(Q(nonfaculty_profile=True)).exists():
 
             return super(FacultyOrLocalAdminBackend, self).authenticate(username=username,
