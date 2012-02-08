d465837d6ada0487f9df9af4591a16bc0d32db03
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-02-08 15:30:01 +0000

    customize user profile display/functionality in django admin site

--- openemory/accounts/admin.py
+++ openemory/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from eullocal.django.emory_ldap.admin import EmoryLDAPUserProfileAdmin
 from openemory.accounts.models import Bookmark, Degree, Position, \
         UserProfile
 
@@ -19,3 +20,20 @@ class PositionAdmin(admin.ModelAdmin):
     list_display = ('holder', 'name')
 
 admin.site.register(Position, PositionAdmin)
+
+
+class UserProfileAdmin(EmoryLDAPUserProfileAdmin):
+    '''Extend
+    :class:`eullocal.django.emory_ldap.admin.EmoryLDAPUserProfileAdmin`
+    to customize for locally added profile fields and functionality.
+    '''
+    list_display = ('__unicode__', 'full_name', 'edit_user',
+                    'show_suppressed', 'nonfaculty_profile')
+    list_filter = ('show_suppressed', 'nonfaculty_profile')
+    list_editable = ('nonfaculty_profile', )
+    search_fields = ('user__username', 'full_name', 'user__last_name', 'user__first_name')
+
+# unregister eullocal user profile admin
+admin.site.unregister(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
+
