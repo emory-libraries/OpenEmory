dc99d2e9b8a53d6c40f10f940a02dfeb37e806a0
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2012-02-13 21:07:52 +0000

    preliminary manage.py command for indexing esd faculty into solr #223

---
+++ openemory/accounts/management/commands/index_faculty.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand, CommandError
+from openemory.accounts.models import EsdPerson
+from openemory.util import solr_interface
+import socket
+from sunburnt import SolrError
+
+
+class Command(BaseCommand):
+    '''Index Faculty ESD information into Solr for searching.
+    '''
+    help = __doc__
+
+    v_normal = 1  # 1 = normal, 0 = minimal, 2 = all
+    
+    def handle(self, verbosity=1, *args, **options):
+
+        if verbosity >= self.v_normal:
+            self.stdout.write('Indexing ESD data for %d faculty members in Solr\n' % \
+                              EsdPerson.faculty.all().count())
+        try:
+            solr = solr_interface()
+        except socket.error as se:
+            raise CommandError('Failed to connect to Solr (%s)' % se)
+
+        try:
+            solr.add(EsdPerson.faculty.iterator(), chunk=10)
+        except SolrError as se:
+            if 'unknown field' in str(se):
+                raise CommandError('Solr unknown field error ' +
+                                   '(check that local schema matches running instance)')
+            raise CommandError('Solr error (%s)' % se)
