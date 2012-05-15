b7e3e09ad581a4502aca8a4b8074eec396fb5841
Ben Ranker <branker@emory.edu>
2012-05-15 18:30:02 +0000

    update index_faculty to reflect removed faculty members; update DEPLOYNOTES to cron up index_faculty

--- openemory/accounts/management/commands/index_faculty.py
+++ openemory/accounts/management/commands/index_faculty.py
@@ -19,12 +19,15 @@ class Command(BaseCommand):
     )
 
     v_normal = 1  # 1 = normal, 0 = minimal, 2 = all
+    v_all = 2
     
     def handle(self, verbosity=1, *args, **options):
 
-        if verbosity >= self.v_normal:
-            self.stdout.write('Indexing ESD data for %d faculty members in Solr\n' % \
-                              EsdPerson.faculty.all().count())
+        self.verbosity = verbosity
+
+        if self.verbosity >= self.v_normal:
+            print 'Indexing ESD data for %d faculty members in Solr' % \
+                    (EsdPerson.faculty.all().count(),)
 
         try:
             solr_url = options.get('index_url', None)
@@ -33,10 +36,48 @@ class Command(BaseCommand):
             raise CommandError('Failed to connect to Solr (%s)' % se)
 
         try:
-            solr.add((p.index_data() for p in EsdPerson.faculty.all()),
-                     chunk=100)
+            self.update_faculty_index(solr)
         except SolrError as se:
             if 'unknown field' in str(se):
                 raise CommandError('Solr unknown field error ' +
                                    '(check that local schema matches running instance)')
             raise CommandError('Solr error (%s)' % se)
+
+    def update_faculty_index(self, solr):
+        active_faculty = set()
+        self.index_faculty(solr, active_faculty)
+        self.remove_deactivated_faculty(solr, active_faculty)
+        solr.commit()
+
+    def index_faculty(self, solr, active_faculty):
+        for p in EsdPerson.faculty.all():
+            if self.verbosity >= self.v_all:
+                print 'Indexing', p.username
+            active_faculty.add(p.username)
+            solr.add(p.index_data())
+
+    def remove_deactivated_faculty(self, solr, active_faculty):
+        PAGE_SIZE = 100
+        page = 0
+        for faculty in self.indexed_faculty(solr):
+            if faculty['username'] not in active_faculty:
+                if self.verbosity >= self.v_all:
+                    print 'Removing deactivated faculty', faculty['username']
+                solr.delete(faculty)
+
+    def indexed_faculty(self, solr):
+        PAGE_SIZE = 100
+        page = 0
+        while True:
+            if self.verbosity >= self.v_all:
+                print 'Fetching faculty page %d from index' % (page,)
+            response = solr.query(record_type=EsdPerson.record_type) \
+                           .paginate(start=page * PAGE_SIZE, rows=PAGE_SIZE) \
+                           .execute()
+            if not list(response):
+                # no more results.
+                return
+            for faculty in response:
+                yield faculty
+
+            page += 1
