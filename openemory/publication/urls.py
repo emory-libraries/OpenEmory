a442e27c216269050117dbf50b2e358d14a0ac03
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2013-01-17 17:39:44 +0000

    adjust article urls so pid requires a : [Fixes #42731863]

--- openemory/publication/urls.py
+++ openemory/publication/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 from openemory.publication import views
 
 urlpatterns = patterns('',
@@ -8,16 +8,18 @@ urlpatterns = patterns('',
     url(r'^search/$', views.search, name='search'),
     url(r'^unreviewed/$', views.review_queue, name='review-list'),
     url(r'^departments/$', views.departments, name='list-departments'),
-    url(r'^(?P<pid>[^/]+)/$', views.view_article, name='view'),
-    url(r'^(?P<pid>[^/]+)/edit/$', views.edit_metadata, name='edit'),
-    url(r'^(?P<pid>[^/]+)/pdf/$', views.download_pdf, name='pdf'),
+    # we could probably be even more explicit with the pid pattern,
+    # but at least require a colon between pidspace and identifier
+    url(r'^(?P<pid>[^:]+:[^/]+)/$', views.view_article, name='view'),
+    url(r'^(?P<pid>[^:]+:[^/]+)/edit/$', views.edit_metadata, name='edit'),
+    url(r'^(?P<pid>[^:]+:[^/]+)/pdf/$', views.download_pdf, name='pdf'),
     # raw datastream view; add other dsids here as appropriate
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>contentMetadata|descMetadata|provenanceMetadata)/$',
+    url(r'^(?P<pid>[^:]+:[^/]+)/(?P<dsid>contentMetadata|descMetadata|provenanceMetadata)/$',
         views.view_datastream, name='ds'),
-    url(r'^(?P<pid>[^/]+)/AUDIT/$', views.audit_trail, name='audit-trail'),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>authorAgreement)/$',
+    url(r'^(?P<pid>[^:]+:[^/]+)/AUDIT/$', views.audit_trail, name='audit-trail'),
+    url(r'^(?P<pid>[^:]+:[^/]+)/(?P<dsid>authorAgreement)/$',
         views.view_private_datastream, name='private_ds'),
-    url(r'^(?P<pid>[^/]+)/biblio/$', views.bibliographic_metadata, name='biblio-data'),
+    url(r'^(?P<pid>[^:]+:[^/]+)/biblio/$', views.bibliographic_metadata, name='biblio-data'),
     url(r'^(?P<field>[a-zA-Z0-9_-]+)/autocomplete/$', views.suggest, name='suggest'),
     url(r'^journal_publisher/details/$', views.publisher_details, name='publisher-details'),
     url(r'^oa-fund/proposal/$', views.open_access_fund, name='oa-fund-form'),
