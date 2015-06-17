aa1268b54efb7ffd356b373b7d0bc503f0badd15
Alex Thomas <athom09@emory.edu>
2015-06-16 22:28:16 -0400

    typeo

--- openemory/settings.py
+++ openemory/settings.py
@@ -70,7 +70,7 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = []
+TEMPLATE_CONTEXT_PROCESSORS = [
     # defaults:
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
