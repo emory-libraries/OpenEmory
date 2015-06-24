fd6031aa3f42cd158effc59fa1a92edc433dc696
Alex Thomas <athom09@emory.edu>
2015-06-24 16:53:56 -0400

    changed user profile lookup logic on home page

--- openemory/publication/templatetags/publication_extras.py
+++ openemory/publication/templatetags/publication_extras.py
@@ -21,6 +21,7 @@ Custom template tags and filters for displaying Publication content.
 from django import template
 from django.template.defaultfilters import stringfilter
 from openemory.util import pmc_access_url
+from django.contrib.auth.models import User
 
 register = template.Library()
 
@@ -42,7 +43,16 @@ def pmc_url(value):
 def parse_author(stored_value):
     '''Parse author data out of a solr parsed_author field.'''
     netid, rest = stored_value.split(':', 1)
+
+    try:
+        user = User.objects.get(username=netid)
+        has_profile =  user.get_profile.has_profile_page()
+    except:
+        has_profile = False
+
     return {
         'netid': netid,
         'name': rest,
+        'hasprofile': has_profile,
+
     }
