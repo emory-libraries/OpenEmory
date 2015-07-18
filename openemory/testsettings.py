d5b54b4b735e9efdb27526681f87f5cd282a8d28
Alex Thomas <athom09@emory.edu>
2015-07-18 17:51:08 -0400

    allow pidman setting to stand for now

--- openemory/testsettings.py
+++ openemory/testsettings.py
@@ -17,10 +17,10 @@
 from settings import *
 
 # remove PIDMAN settings - no need to generate PIDs when testing
-PIDMAN_HOST = None
-PIDMAN_USER = None
-PIDMAN_PASSWORD = None
-PIDMAN_DOMAIN = None
+#PIDMAN_HOST = None
+#PIDMAN_USER = None
+#PIDMAN_PASSWORD = None
+#PIDMAN_DOMAIN = None
 
 # Disable statistics during testing. The statistics processors query solr to
 # get current article and faculty statistics. This is great for the live
