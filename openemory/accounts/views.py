36a6ea37fa4637a75e5db673ade54b06cf1ad936
Alex Zotov <alexandr.zotov@emory.edu>
2015-09-14 15:20:32 -0400

    checking against ESD for first and last name

--- openemory/accounts/views.py
+++ openemory/accounts/views.py
@@ -601,7 +601,7 @@ def faculty_autocomplete(request):
     # omit the less relevant items)
     print r
     suggestions = [
-        {'label': u.get('first_name', '').split()[0] + " " + u.get('last_name', ''),  # directory name in lastname, firstname format
+        {'label':  EsdPerson.objects.get(netid=u['username'].upper()).directory_name,  # directory name in lastname, firstname format
          'description': u.get('department_name', ''),  # may be suppressed
          'username': u['username'],
          # first name is missing in some cases-- don't error if it's not present
