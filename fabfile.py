d7ce5d585bb2d1e8c3a7be6a18942545ff19baf6
Alex Zotov <alexandr.zotov@emory.edu>
2016-07-05 11:44:47 -0400

    keepdb option to fab

--- fabfile.py
+++ fabfile.py
@@ -54,7 +54,7 @@ def test():
     app_list = ['accounts', 'common', 'publication', 'harvest']
     apps2test = ' '.join(map(lambda app: env.project + '.' + app +'.tests', app_list))
 
-    testing_cmd = 'python manage.py test --with-coverage --cover-package=%(project)s --cover-xml --with-xunit ' %env
+    testing_cmd = 'python manage.py test --keepdb --with-coverage --cover-package=%(project)s --cover-xml --with-xunit ' %env
     testing_cmd += apps2test
     local(testing_cmd)
 
