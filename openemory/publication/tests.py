2aea860b8a9a150ec20c5639942f52af3b45fa45
Alex Zotov <alexandr.zotov@emory.edu>
2016-08-03 13:17:51 -0400

    bugs in pubs tests

--- openemory/publication/tests.py
+++ openemory/publication/tests.py
@@ -1769,7 +1769,7 @@ class PublicationViewsTest(TestCase):
         #set save-record flag should cause additional fields to become optional
         data['save-record'] = True
         response = self.client.post(edit_url, data)
-        if response.status_code = 303: # no context data if redirect
+        if response.status_code != 303: # no context data if redirect
             self.assert_('invalid_form' not in response.context,
                          'posted form data should not result in an invalid form')
 
